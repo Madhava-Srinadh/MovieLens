7fcb8be6e15baa4fdd155b1eb9ba4d772e444d16
Madhava-Srinadh <madhavasrinadh18@gmail.com>
2025-02-26 13:06:17 +0530

    Fixed keys

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDOdKPAXFQ7MA5JM_96jeYjGDVUE6q3anY",
-  authDomain: "movielens18.firebaseapp.com",
-  projectId: "movielens18",
-  storageBucket: "movielens18.firebasestorage.app",
-  messagingSenderId: "836939121600",
-  appId: "1:836939121600:web:ef9fdcd06bb153cbfa7e25",
-  measurementId: "G-JDQQF8T1XK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
